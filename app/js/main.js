17248f97edf7f75a1ddd5aa96e3aab84de544bb4
lvmzcndvld-dev <infiniti.solitude@gmail.com>
2020-11-20 18:20:30 +0300

    added products and fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,15 @@ $(function () {
     $('.top-slider__items').slick({
         dots: true,
         arrows: false,
-        autoplay: true,
-        autoplaySpead: 3000
+        // autoplay: true,
+        // autoplaySpead: 3000
     });
+
+    $('.products__item').hover (
+        function () {
+            $(this).find('.group-icons').toggleClass('group-icons--hover');
+        }
+    );
+
+    var mixer = mixitup('.products__content');
 });
